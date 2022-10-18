8099976690853b771182148a43e5fc0773db82be
nagarajugubbala <nraj888@gmail.com>
2022-10-18 17:52:21 +0530

    added fileds in registration and design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ export class AppComponent {
   links = ['home', 'about', 'service', 'projects', 'corevalues', 'contact'];
   titles = ['Home', 'About Us', 'Service', 'Projects', 'Core Values', 'Contact Us'];
   activeLink = this.links[0];
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  opened!: boolean;
+  clickHandler() {
+    this.sidenav.close();
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StepperOrientation } from '@angular/material/stepper';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -10,15 +13,40 @@ export class SignupComponent implements OnInit {
   companyDetails!: FormGroup;
   personalDetails!: FormGroup;
   projectDetails!: FormGroup;
+  typeOfEstablishmentsList: string[] = ['Private Limited Company', 'Public Limited Company', 'Partnership',
+    'Limited Liability Partnership', 'One Person Company'];
+  states: string[] = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+  ];
+  countries: string[] = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+  ];
+  typeOfWorksList: string[] = [
+    'Civil',
+    'Electrical',
+  ];
 
-  constructor(private _formBuilder: FormBuilder) { }
+  stepperOrientation!: Observable<StepperOrientation>;
+  constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
 
   ngOnInit(): void {
     this.companyDetails = this._formBuilder.group({
+      users: ['', Validators.required],
       company_name: ['', [Validators.required, Validators.maxLength(20)]],
       year_of_establishment: ['', Validators.required],
       type_of_establishment: ['', Validators.required],
       address: ['', [Validators.required, Validators.maxLength(200)]],
+      type_of_work: ['', Validators.required],
     });
     this.personalDetails = this._formBuilder.group({
       name: ['', Validators.required],
