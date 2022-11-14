21123cb339dfb51080dbe7d24b97da41893aa066
nagarajugubbala <nraj888@gmail.com>
2022-11-14 20:20:29 +0530

    feat(create-tender.ts): design changes
    design changes in create tender component

--- src/app/components/create-tender/create-tender.component.ts
+++ src/app/components/create-tender/create-tender.component.ts
@@ -1,7 +1,19 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+export interface Element {
+  position: string;
+  item_description: string;
+  unit: string;
+  quantity: string;
+}
 
+const ELEMENT_DATA: Element[] = [
+  { position: '1', item_description: 'STRUCTURAL STEEL WORK', unit: 'Ton', quantity: '20' },
+  { position: '2', item_description: 'STRUCTURAL STEEL WORK', unit: 'Kilo', quantity: '40' },
+];
 @Component({
   selector: 'app-create-tender',
   templateUrl: './create-tender.component.html',
@@ -9,9 +21,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class CreateTenderComponent implements OnInit {
   tenderDetails!: FormGroup;
-
+  ftdTableRows!: FormGroup;
   constructor(private _formBuilder: FormBuilder, private _snackBar: MatSnackBar) { }
-
+  
   ngOnInit(): void {
     this.tenderDetails = this._formBuilder.group({
       type_of_work: ['', Validators.required],
@@ -21,27 +33,70 @@ export class CreateTenderComponent implements OnInit {
       contract_duration: ['', [Validators.required]],
       date_of_submission: ['', [Validators.required]],
       budget: ['', [Validators.required]],
-      technical_tender_form: [''],
+      file: ['', Validators.required],
+      fileSource: ['', Validators.required],
+      ftdTableRows: this._formBuilder.group({
+        position: ['', Validators.required],
+        item_description: ['', [Validators.required]],
+        unit: ['', Validators.required],
+        quantity: ['', Validators.required]
+      })
     });
   }
 
-
-  // file upload reference
-  // https://stackblitz.com/edit/file-upload-drag-n-drop?file=src%2Fapp%2Fapp.component.ts
-
-  public files: any[] = [];
-  onFileChange(pFileList: any) {
-    this.files = Object.keys(pFileList).map(key => pFileList[key]);
-    this._snackBar.open("Successfully upload!", 'Close', {
-      duration: 2000,
-    });
+  isFileUploaded = false;
+  file: any;
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      // console.log(this.file.name);
+      this.tenderDetails.patchValue({
+        fileSource: this.file
+      });
+    }
+  }
+  removeSelectedFile(f: any) {
+    if (f) {
+      this.file = '';
+    }
   }
 
-  deleteFile(f:any) {
-    this.files = this.files.filter(function (w) { return w.name != f.name });
-    this._snackBar.open("Successfully delete!", 'Close', {
-      duration: 2000,
-    });
-  }
+  typeOfWorks = ['Consultancy Services',
+    'Structure Works (Civil)',
+    'Structure Works (Steel/PEB/Precast)',
+    'Structural Post Tensioning Works',
+    'Interior Finishes',
+    'Interior Fitout Works',
+    'Elevation Façade / Glazing',
+    'Waterproofing Works',
+    'Building Electrical Works (LT)',
+    'Building Electrical Works (HT)',
+    'Fire Fighting System Installation',
+    'HVAC Systems',
+    'Elevators',
+    'Sewage Treatment Plants (STP)',
+    'Water Treatment Plants (WTP)',
+    'Landscaping',
+    'Diesel Generators',
+    'IBMS (Integrated Building Management System)',
+    'CCTV, Access Control, Security & Surveilance Systems',
+    'Building Automation Systems',
+    'Compressed Air Systems',
+    'LPG/LNG Gas Distribution Systems',
+    'Nurse Call Systems',
+    'Medical Gas Systems',
+    'Pneumatic Tube Systems'
+  ];
+  public typeOfWorksList = this.typeOfWorks.slice();
 
+  typeOfContracts = ['Itemised Fixed Rate Contract',
+    'Fixed Lumpsum Price Contract',
+    'Design & Build Contract',
+    'GMP Contract',
+    'Cost Plus Percentage Contract'
+  ]
+  public typeOfContractsList = this.typeOfContracts.slice();
+
+  displayedColumns: string[] = ['position', 'item_description', 'unit', 'quantity'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/directives/drag-drop-file-upload.directive.ts
+++ src/app/directives/drag-drop-file-upload.directive.ts
@@ -5,7 +5,7 @@ import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@ang
 })
 export class DragDropFileUploadDirective {
 
-  @Output() private filesChangeEmiter: EventEmitter<any> = new EventEmitter();
+  @Output() private filesChangeEmiter: EventEmitter<File[]> = new EventEmitter();
   @HostBinding('style.background') private background = '#eee';
   @HostBinding('style.border') private borderStyle = '2px dashed';
   @HostBinding('style.border-color') private borderColor = '#696D7D';
@@ -13,30 +13,30 @@ export class DragDropFileUploadDirective {
 
   constructor() { }
 
-  @HostListener('dragover', ['$event']) public onDragOver(evt: any) {
-    evt.preventDefault();
-    evt.stopPropagation();
+  @HostListener('dragover', ['$event']) public onDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     this.background = 'lightgray';
     this.borderColor = 'cadetblue';
     this.borderStyle = '3px solid';
   }
 
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
-    evt.preventDefault();
-    evt.stopPropagation();
+  @HostListener('dragleave', ['$event']) public onDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     this.background = '#eee';
     this.borderColor = '#696D7D';
     this.borderStyle = '2px dashed';
   }
 
-  @HostListener('drop', ['$event']) public onDrop(evt: any) {
-    evt.preventDefault();
-    evt.stopPropagation();
+  @HostListener('drop', ['$event']) public onDrop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     this.background = '#eee';
     this.borderColor = '#696D7D';
     this.borderStyle = '2px dashed';
-    let files = evt.dataTransfer.files;
-    let valid_files: Array<File> = files;
+    let files = event.dataTransfer.files;
+    let valid_files : Array<File> = files;
     this.filesChangeEmiter.emit(valid_files);
   }
 
