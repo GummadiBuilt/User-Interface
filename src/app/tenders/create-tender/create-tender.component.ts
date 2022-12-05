f4d1de66f839ac2774850fa4597637979a87ece3
Sri Harsha,Bhasuru <sriharsha.bhasuru@gmail.com>
2022-12-05 13:42:33 -0600

    fix(create-tender): disable tender on submit
    Disable tender edit on publish for admins

--- src/app/tenders/create-tender/create-tender.component.ts
+++ src/app/tenders/create-tender/create-tender.component.ts
@@ -407,7 +407,7 @@ export class CreateTenderComponent implements OnInit {
   }
   tenderFormDisable() {
     if (this.userRole?.includes("client") && (this.tenderDetails.get('workflowStep')?.value == 'Yet to be published'
-          || this.tenderDetails.get('workflowStep')?.value == 'YET_TO_BE_PUBLISHED')) {
+          || this.tenderDetails.get('workflowStep')?.value == 'Published')) {
       // console.log('inside',this.tenderDetails.controls['workflowStep'].value);
       this.tenderDetails.disable();
       this.btnstate = true;
