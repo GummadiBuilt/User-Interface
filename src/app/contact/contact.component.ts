5ff573d588f9e082f6cb46805413d3bb05adc270
nagarajugubbala <nraj888@gmail.com>
2023-02-16 18:40:03 +0530

    feat(contact.component.ts): contact us page design
    added contact form in contact us page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contactUsForm!: FormGroup;
+  
+  constructor(private _formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.contactUsForm = this._formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      category: [null, Validators.required],
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      phone: [null, [Validators.required, Validators.pattern("^[0-9]*$"),
+      Validators.minLength(10), Validators.maxLength(10)]],
+      description: [null, [Validators.required, Validators.maxLength(250)]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.contactUsForm.value);
   }
 
 }
